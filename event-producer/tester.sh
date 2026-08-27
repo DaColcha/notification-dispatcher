23ac0464a53a59eea040c8f9cc0be6ff8b12cbd2
DaColcha <da.colcha@hotmail.com>
2026-08-27 13:10:24 -0500

    chores: update tester messages and adding config files

--- event-producer/tester.sh
+++ event-producer/tester.sh
@@ -1,11 +1,11 @@
 curl -X POST "http://localhost:8081/event" \
   -H "Content-Type: application/json" \
-  -d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"EMAIL","destination":"email-test@gmail.com","message":"Random event notification"}'
+  -d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"EMAIL","destination":"da.colcha@gmail.com","message":"Test test on email "}'
 
 curl -X POST "http://localhost:8081/event" \
 -H "Content-Type: application/json" \
--d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"SLACK","destination":"email-test@gmail.com","message":"Random event SLACK notification"}'
+-d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"SLACK","destination":"email-test@gmail.com","message":"Test on SLACK ------ test "}'
 
 curl -X POST "http://localhost:8081/event" \
   -H "Content-Type: application/json" \
-  -d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"DISCORD","destination":"email-test@gmail.com","message":"Random event  DISCORD notification"}'
+  -d '{"eventId":"259d39e6-9b5f-4b26-bc2e-8c6d87ef14a0","eventType":"DISCORD","destination":"email-test@gmail.com","message":"Test on discord uwu "}'
